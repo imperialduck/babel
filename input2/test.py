1ac12bf461e7ca565664dbfdabc1c4929bdc4fdf
Leo Malaterre <malaterre.leo.lm@gmail.com>
2019-12-18 16:22:48 +0100

    Convert to actual unit tests

--- input2/test.py
+++ input2/test.py
@@ -1,4 +1,4 @@
-import year
+from year import validate_year
 import unittest
 
 
@@ -7,10 +7,17 @@ class YearTest(unittest.TestCase):
     Tests for year.py
     """
     def test_year(self):
-        """Test that input_year is INT"""
-        self.assertEqual(type(year.validate_year(1987)), int)
-        self.assertEqual(type(year.validate_year("1987")), int)
-        self.assertNotEqual(type(year.validate_year("lxlw<kjx")), int)
+        """
+        
+        """
+        
+        self.assertEqual(validate_year(1987), 1987)
+        self.assertEqual(validate_year("1987"), 1987)
+        self.assertEqual(validate_year("lxlw<kjx"), None)
+        self.assertEqual(validate_year("03"), 2003)
+        self.assertEqual(validate_year("69"), 1969)
+        self.assertEqual(validate_year("20"), 1920)
+        self.assertEqual(validate_year("2020"), None)
 
 
 if __name__ == "__main__":
