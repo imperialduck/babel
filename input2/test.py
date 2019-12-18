95edfd709ee79f967e89882465c814dd21353294
Leo Malaterre <malaterre.leo.lm@gmail.com>
2019-12-18 15:04:59 +0100

    correct error

--- input2/test.py
+++ input2/test.py
@@ -8,4 +8,4 @@ t_year = year.validate_year(1987)
 print(t_year, type(t_year))  # expected output : 1987, <class 'int'>
 
 t_year = year.validate_year("qdkldsjq")
-print(t_year, type(t_year))  # expected output : 1987, <class 'NoneType'>
+print(t_year, type(t_year))  # expected output : None, <class 'NoneType'>
