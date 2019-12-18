803c7fcfa26a52736b0cde83e0693b31e9d1d99c
Leo Malaterre <malaterre.leo.lm@gmail.com>
2019-12-18 15:06:25 +0100

    corrected errors in test.py Just an expected output written incorrectly

--- input2/test.py
+++ input2/test.py
@@ -2,10 +2,10 @@ import year
 
 
 t_year = year.validate_year("1987")
-print(t_year, type(t_year))  # expected output : 1987, <class 'int'>
+print(t_year, type(t_year))  # expected output: 1987, <class 'int'>
 
 t_year = year.validate_year(1987)
-print(t_year, type(t_year))  # expected output : 1987, <class 'int'>
+print(t_year, type(t_year))  # expected output: 1987, <class 'int'>
 
 t_year = year.validate_year("qdkldsjq")
-print(t_year, type(t_year))  # expected output : None, <class 'NoneType'>
+print(t_year, type(t_year))  # expected output: None, <class 'NoneType'>
