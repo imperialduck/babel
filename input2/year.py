06d70080a3c48f2275e2386595822c99fbeffac8
Leo Malaterre <malaterre.leo.lm@gmail.com>
2019-12-18 16:23:36 +0100

    Refactoring and handling of new errors

--- input2/year.py
+++ input2/year.py
@@ -2,6 +2,8 @@
 
 import datetime
 
+DATE = datetime.datetime.now()
+
 
 def validate_year(year):
     """
@@ -9,14 +11,22 @@ def validate_year(year):
         year: STRING
     }
     Validates that a string is convertible to INT and converts it
+    Then, checks if year is 2 digits and converts it to a 4 digits date if its not in the future
     """
+    length = len(str(year))
     try:
         int(year)
     except Exception:
+        print("L'année de naissance ne doit comporter que des chiffres")
         year = None
         return year
     else:
         year = int(year)
+        if length <= 2:
+            year = check_age(year)
+        elif year > DATE.year:
+            year = None
+            print("Nom de Zeus ! Marty, nous sommes revenus en {}".format(DATE.year))
         return year
 
 
@@ -28,8 +38,7 @@ def check_age(year):
     If the two digits date entered is at most 20 years old and not in the future, place it in the 1900's
     Else, place it in the 2000's
     """
-    date = datetime.datetime.now()
-    if (year + 1900) >= (date.year - 20) or ((year + 2000) > date.year):
+    if (year + 1900) >= (DATE.year - 20) or ((year + 2000) > DATE.year):
         return year + 1900
     else:
         return year + 2000
@@ -43,21 +52,15 @@ def controller_input():
     while user_input == '':
         user_input = input("Entrez votre année de naissance (format yy ou yyyy)")
         input_len = len(user_input)
-        if user_input[0] == "0" and input_len >= 2:
-            user_input = user_input[1]  # Strip the 0 at the beginning of the string as it will prevent any conversion to INT and crash the program
 
         user_input = validate_year(user_input)
-        if user_input:
-            if input_len == 2:
-                user_input = check_age(user_input)
-                return user_input
-            elif input_len == 4:
+        if input_len == 2 or input_len == 4:
+            if user_input:
                 return user_input
             else:
-                print("Erreur, votre année de naissance doit comporter 2 ou 4 chiffres")
                 user_input = ''
         else:
-            print("L'année de naissance ne doit comporter que des chiffres")
+            print("Erreur, votre année de naissance doit comporter 2 ou 4 chiffres")
             user_input = ''
 
 
